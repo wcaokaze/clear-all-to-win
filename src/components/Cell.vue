2bdb724d468a349aa6a6da16c7ce77aa5b7427c0
wcaokaze <wcaokaze@gmail.com>
2020-06-15 03:12:14 +0900

    とりあえずセルを3つ並べてみました

---
+++ src/components/Cell.vue
@@ -0,0 +1,26 @@
+<template>
+    <span v-bind:class="{ live: isLive, dead: !isLive }"></span>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class Cell extends Vue {
+        @Prop() private isLive?: boolean;
+    }
+</script>
+
+<style scoped lang="stylus">
+    span {
+        background-clip padding-box
+
+        &.live {
+            background-color aquamarine
+        }
+
+        &.dead {
+            background-color gray
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div class="field">
+            <Cell class="cell" />
+            <Cell class="cell"/>
+            <Cell class="cell"/>
+        </div>
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import Cell from '@/components/Cell.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    @Component({
+        components: {
+            Cell
+        }
+    })
+    export default class Home extends Vue {}
 </script>
+
+<style lang="stylus">
+    .field {
+        width 100%
+        display flex
+        flex-direction row
+        flex-wrap nowrap
+        justify-content center
+
+        .cell {
+            width  48px
+            height 48px
+            margin 4px
+        }
+    }
+</style>
