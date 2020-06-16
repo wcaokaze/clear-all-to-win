ffb5d665bfed7440400ce089f342e4cb0fd13318
wcaokaze <wcaokaze@gmail.com>
2020-06-17 02:29:33 +0900

    絶対クリアできるパターンしかでなくした

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,17 +23,24 @@
         ];
 
         mounted() {
-            this.generateRandomField(5, 5);
+            this.field = Home.generateRandomField(5, 5, 15, this.rule);
         }
 
         private onCellClick(clickedX: number, clickedY: number) {
             const field = this.field.concat();
-            const rule = this.rule;
+            Home.invert(field, clickedX, clickedY, this.rule);
+            this.field = field;
+        }
 
+        private static invert(field: boolean[][],
+                              targetX: number,
+                              targetY: number,
+                              rule: boolean[][])
+        {
             for (let dx = -1; dx <= 1; dx++) {
                 for (let dy = -1; dy <= 1; dy++) {
-                    const x = clickedX + dx;
-                    const y = clickedY + dy;
+                    const x = targetX + dx;
+                    const y = targetY + dy;
 
                     if (x < 0 || x >= field.length) { continue; }
                     if (y < 0 || y >= field.length) { continue; }
@@ -43,16 +50,30 @@
                     }
                 }
             }
-
-            this.field = field;
         }
 
-        private generateRandomField(columnCount: number, rowCount: number) {
-            this.field = Array.from(Array(columnCount), () =>
-                Array.from(Array(rowCount), () =>
-                    Math.random() > 0.5
+        private static generateRandomField(
+            columnCount: number,
+            rowCount: number,
+            complexity: number,
+            rule: boolean[][]
+        ): boolean[][] {
+            const field = Array.from(
+                Array(columnCount),
+                () => Array.from(
+                    Array(rowCount),
+                    () => false
                 )
             );
+
+            for (let i = 0; i < complexity; i++) {
+                const x = Math.floor(Math.random() * columnCount);
+                const y = Math.floor(Math.random() * rowCount);
+
+                this.invert(field, x, y, rule);
+            }
+
+            return field;
         }
     }
 </script>
